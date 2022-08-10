d3024d7dec5d65332dea5d991f08978b341c0672
pippimotta <lingyizh84@gmail.com>
2022-08-10 21:19:25 +0900

    remove cache

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -51,6 +51,27 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func (m *Repository) Test(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "test.page.tmpl", &models.TemplateData{})
+//render the reservation page and the form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+//renders the room page general
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+//render the room page major
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+//search availability
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+//contact
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
 }
