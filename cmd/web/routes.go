0c2984d70c56e6423560c4e39ff0e66e6b051aef
pippimotta <lingyizh84@gmail.com>
2022-08-10 21:13:48 +0900

    add:file server

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,18 +10,23 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	// mux.Get("/test", http.HandlerFunc(handlers.Repo.Test))
 
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-	mux.Get("/test", handlers.Repo.Test)
+	mux.Get("/generals", handlers.Repo.Generals)
+	mux.Get("/majors", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	//remember to set in route.go to serve file
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
